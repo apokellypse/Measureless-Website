8597da01c970d1d4ecb7e1442bea07b846d65232
myuh <cg447@cornell.edu>
2014-11-02 21:17:30 -0500

    Index.php
    added html format

---
+++ index.php
@@ -0,0 +1,17 @@
+
+
+<body>
+
+<php? include "nav.php"></php?>    
+<php? include "header.php"></php?>    
+
+<div class="content" id="slideshow">
+    </div>
+    
+<div class="content" id="announcements">
+    
+    </div>
+    
+<php? include "footer.php"></php?>
+    
+</body>

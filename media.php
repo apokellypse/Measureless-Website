cc82b7a425395a78ea9a9c8543c4d7a6dab8f1e1
Kelly Yu <euriiko@gmail.com>
2014-11-03 12:48:45 -0500

    added YouTube links to media.php

--- media.php
+++ media.php
@@ -16,6 +16,41 @@
 	<div class="content" id="announcements">
 	    
 	    </div>
+
+	<!--Start content here-->
+
+	<!--YouTube links here-->
+	<iframe src="http://youtu.be/ep5MQ6N32LE"></iframe>
+	<p>"No One / Where is the Love" soloists Andrew Park, Faith Wang, and Charlene Tsai</p>
+	<p>2014 Asia Night at Cornell</p>
+
+	<iframe src="http://youtu.be/oamZ1-e1nLY"></iframe>
+	<p>“Beautiful You” soloists Joyce Wang and William Loo</p>
+	<p>2014 Asia Night at Cornell</p>
+
+	<iframe src="http://youtu.be/xkQVtV5dMik"></iframe>
+	<p>“Falling Slowly” performed by Frank and Faith</p>
+	<p>2014 Spring Concert</p>
+
+	<iframe src="http://youtu.be/r-MoAzhZjdY"></iframe>
+	<p>“Killing Me Softly”</p>
+	<p>2012 Break It Down Boston</p>
+
+	<iframe src="http://youtu.be/skZjGGgyPNo"></iframe>
+	<p>“Reborn/We Found Love”</p>
+	<p>2012 Break It Down Boston</p>
+
+	<iframe src="http://youtu.be/bnrHxje9gRU"></iframe>
+	<p>“Beautiful You”</p>
+	<p>2011 Break It Down Boston</p>
+
+	<!--If provided, music samples here-->
+
+	<!--If provided, recruiting video here-->
+
+	<!--If provided, group pictures / candids here-->
+
+	<!--If provided, old posters / promotional items maybe here-->
 	    
 	<?php include "footer.php";?>
 	    

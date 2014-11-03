554b334ee1242c87491791f52ed65d35f2095614
Kelly Yu <euriiko@gmail.com>
2014-11-03 15:04:37 -0500

    updated meta information to include basics

---
+++ meta.php
@@ -0,0 +1,13 @@
+<?php
+echo '<meta charset="utf-8">
+	<meta name="keywords" content="measureless, cornell, Cornell, Measureless, acapella, a capella, music, choir, singing">
+	<meta name="description" content="Measureless is a Christian A Capella group at Cornell University.">
+	<meta name="author" content="Christine Geeng, Aaron Coven, Manu Rathore, Kelly Yu">
+	<title>Cornell Measureless A Capella</title>'.
+	//this is for the tab icon
+	'<link rel="icon" type="image/ico" href="">'.
+	//this is for stylesheets
+	'<link rel="stylesheet" href="">'.
+	//this is for scripts
+	'<script src=""></script>'
+?>

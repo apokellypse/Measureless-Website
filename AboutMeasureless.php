69de76cccf5fa51110b86dacb93580320595cf57
Aaron Coven <acoven@Aarons-MacBook-Pro-2.local>
2014-11-08 10:26:43 -0500

    Made the about page.

---
+++ AboutMeasureless.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+
+	<html>
+
+		<head>
+			<meta charset="utf-8">
+			<title>
+				About Measureless
+			</title>
+		</head>
+
+		<body>
+			<div>
+				<h1>
+					Mission Statement (Purpose and Vision)</h1>
+				<p>
+					Because of the hope we have in Jesus Christ, we seek to glorify God by proclaiming his measureless love and salvation, by worshipping Him with our voices and testimonies, and by serving the Cornell community and beyond through a cappella music.</p>
+				<h1>History</h1>
+				<p>
+					Measureless is by no means new here at Cornell. In fact, Measureless was formed over a decade ago as the fusion of two of Cornell's other acapella groups: The Atonements and The Grace Notes. Since its inception, Measureless has performed at coffeehouses and concerts around campus, traveled to colleges from Pittsburgh to Boston, and had arch-sings with groups such as The Class Notes and Last Call.<br> 
+					Over the course of its lifetime, Measureless' personality, and style have evolved with its members year by year. However, two things have remained constant all throughout--its mission and its God. With God as our rock, purpose, and inspiration, we hope to be a blessing and a light to the Cornell community and beyond for many more years to come.</p>
+				<h1>Why "Measureless"?</h1>
+				<p>
+					Our name comes from the hymn, "The Love of God," which was mostly written by a failed businessman named Frederick M. Lehman in 1917. We say "mostly" because the words of the third stanza came directly from a poem scribbled some 200 years earlier by a prisoner on his cell wall. After the hymn was written, it was discovered that the poem was originally written in Hebrew by a Jewish rabbi named Meir Ben Issac Nehoria around the year 1000.<br>
+					This is the beauty that our a capella group celebrates - God's ability to simultaneously be inspiration, grace, hope, and love to a rabbi, a prisoner, a businessman, and a group of students, separated by centuries, oceans, and cultures. We truly believe with all of our being that our God's love is measureless, and it is this trait for which we are named.</p>
+			</div>
+		</body>
+	</html>

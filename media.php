afb3dde8aa501f0d64b42b1d6de1ba8ed2c456c5
Kelly Yu <euriiko@gmail.com>
2014-11-10 23:41:31 -0500

    formatted media page slightly

--- media.php
+++ media.php
@@ -8,34 +8,44 @@
 	<body>
 
 		<?php include "nav2.php";?>    
-		<div class="content" id="first">
+		<div class="content videos" id="first">
 
-		<!--Start content here-->
+			<!--Start content here-->
 
-		<!--YouTube links here-->
-		<iframe src="//www.youtube.com/embed/ep5MQ6N32LE" frameborder="0" allowfullscreen></iframe>
-		<p>"No One / Where is the Love" soloists Andrew Park, Faith Wang, and Charlene Tsai</p>
-		<p>2014 Asia Night at Cornell</p>
+			<!--YouTube links here-->
+			<iframe src="//www.youtube.com/embed/ep5MQ6N32LE" frameborder="0" allowfullscreen></iframe>
+			<p>"No One / Where is the Love" soloists Andrew Park, Faith Wang, and Charlene Tsai</p>
+			<p>2014 Asia Night at Cornell</p>
 
-		<iframe src="//www.youtube.com/embed/oamZ1-e1nLY" frameborder="0" allowfullscreen></iframe>
-		<p>“Beautiful You” soloists Joyce Wang and William Loo</p>
-		<p>2014 Asia Night at Cornell</p>
+			<hr>
 
-		<iframe src="//www.youtube.com/embed/xkQVtV5dMik" frameborder="0" allowfullscreen></iframe>
-		<p>“Falling Slowly” performed by Frank and Faith</p>
-		<p>2014 Spring Concert</p>
+			<iframe src="//www.youtube.com/embed/oamZ1-e1nLY" frameborder="0" allowfullscreen></iframe>
+			<p>“Beautiful You” soloists Joyce Wang and William Loo</p>
+			<p>2014 Asia Night at Cornell</p>
 
-		<iframe src="//www.youtube.com/embed/r-MoAzhZjdY" frameborder="0" allowfullscreen></iframe>
-		<p>“Killing Me Softly”</p>
-		<p>2012 Break It Down Boston</p>
+			<hr>
 
-		<iframe src="//www.youtube.com/embed/skZjGGgyPNo" frameborder="0" allowfullscreen></iframe>
-		<p>“Reborn/We Found Love”</p>
-		<p>2012 Break It Down Boston</p>
+			<iframe src="//www.youtube.com/embed/xkQVtV5dMik" frameborder="0" allowfullscreen></iframe>
+			<p>“Falling Slowly” performed by Frank and Faith</p>
+			<p>2014 Spring Concert</p>
 
-		<iframe src="//www.youtube.com/embed/bnrHxje9gRU" frameborder="0" allowfullscreen></iframe>
-		<p>“Beautiful You”</p>
-		<p>2011 Break It Down Boston</p>
+			<hr>
+
+			<iframe src="//www.youtube.com/embed/r-MoAzhZjdY" frameborder="0" allowfullscreen></iframe>
+			<p>“Killing Me Softly”</p>
+			<p>2012 Break It Down Boston</p>
+
+			<hr>
+
+			<iframe src="//www.youtube.com/embed/skZjGGgyPNo" frameborder="0" allowfullscreen></iframe>
+			<p>“Reborn/We Found Love”</p>
+			<p>2012 Break It Down Boston</p>
+
+			<hr>
+
+			<iframe src="//www.youtube.com/embed/bnrHxje9gRU" frameborder="0" allowfullscreen></iframe>
+			<p>“Beautiful You”</p>
+			<p>2011 Break It Down Boston</p>
 	        
 	    </div>
 

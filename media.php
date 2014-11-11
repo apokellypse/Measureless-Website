dbfe68a94c3db026b2882a3593fa5b640f6d7960
Kelly Yu <euriiko@gmail.com>
2014-11-10 22:51:57 -0500

    fixed broken link, fixed Youtube embeds, deleted duplicate file

--- media.php
+++ media.php
@@ -15,27 +15,27 @@
 	<!--Start content here-->
 
 	<!--YouTube links here-->
-	<iframe src="http://youtu.be/ep5MQ6N32LE"></iframe>
+	<iframe src="//www.youtube.com/embed/ep5MQ6N32LE" frameborder="0" allowfullscreen></iframe>
 	<p>"No One / Where is the Love" soloists Andrew Park, Faith Wang, and Charlene Tsai</p>
 	<p>2014 Asia Night at Cornell</p>
 
-	<iframe src="http://youtu.be/oamZ1-e1nLY"></iframe>
+	<iframe src="//www.youtube.com/embed/oamZ1-e1nLY" frameborder="0" allowfullscreen></iframe>
 	<p>“Beautiful You” soloists Joyce Wang and William Loo</p>
 	<p>2014 Asia Night at Cornell</p>
 
-	<iframe src="http://youtu.be/xkQVtV5dMik"></iframe>
+	<iframe src="//www.youtube.com/embed/xkQVtV5dMik" frameborder="0" allowfullscreen></iframe>
 	<p>“Falling Slowly” performed by Frank and Faith</p>
 	<p>2014 Spring Concert</p>
 
-	<iframe src="http://youtu.be/r-MoAzhZjdY"></iframe>
+	<iframe src="//www.youtube.com/embed/r-MoAzhZjdY" frameborder="0" allowfullscreen></iframe>
 	<p>“Killing Me Softly”</p>
 	<p>2012 Break It Down Boston</p>
 
-	<iframe src="http://youtu.be/skZjGGgyPNo"></iframe>
+	<iframe src="//www.youtube.com/embed/skZjGGgyPNo" frameborder="0" allowfullscreen></iframe>
 	<p>“Reborn/We Found Love”</p>
 	<p>2012 Break It Down Boston</p>
 
-	<iframe src="http://youtu.be/bnrHxje9gRU"></iframe>
+	<iframe src="//www.youtube.com/embed/bnrHxje9gRU" frameborder="0" allowfullscreen></iframe>
 	<p>“Beautiful You”</p>
 	<p>2011 Break It Down Boston</p>
         
--- nav2.php
+++ nav2.php
@@ -4,7 +4,7 @@
     <li> <a href="about.php"> about</a></li>
         <li><a href="member.php"> members</a></li>
 
-    <li><a href="index.html"><img src="img/note.png" alt="note"></a></li>
+    <li><a href="index.php"><img src="img/note.png" alt="note"></a></li>
 
     <li><a href="media.php">media</a></li>
     <li><a href="getinvolved.php">get involved</a></li>

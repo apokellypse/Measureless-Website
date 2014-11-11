29ed77a77fda72d8d3269273973831a37b02711b
myuh <cg447@cornell.edu>
2014-11-11 10:42:07 -0500

    Revert "manu's stuff"
    This reverts commit 2ec3b6f53197c93339756ad611801b4d91ef460b.

--- getinvolved.php
+++ getinvolved.php
@@ -1,39 +1,33 @@
 <!DOCTYPE html>
-<html>
-	<head>
+
+	<html>
+
+		<head>
 		<?php include "meta.php";?>
-        <title>Measureless Acapella</title>
-        <script src="script/jquery-1.9.1.min.js"></script>
-        <script src="script/jssor.slider.mini.js"></script>
-        <script>
-            jQuery(document).ready(function ($) {
-            var options = { $AutoPlay: true };
-            var jssor_slider1 = new $JssorSlider$('slider1_container', options);
-            });
-        </script>
-  	</head>
+			<title>
+				About Measureless
+			</title>
+		</head>
 
-	<body>
-        <?php include "nav2.php";?>    
+		<body>
+            
+	<?php include "nav2.php";?>    
+ 
+			<div class="content" id="first">
+				<h2>
+					Get Involved</h2>
+				<p>
+					
+Contact Information
+	Primary email: cornell.measureless@gmail.com
+	President: Andrew Park akp67@cornell.edu
+	Public Relations: Charlie Qian cq38@cornell.edu	
 
+Club Links (tumblr, g+, facebook, twitter, youtube…)
+Facebook: https://www.facebook.com/CornellMeasureless </p>
+                </div>
 
-
-    	<div class="content" id="getinvolved">
-	        <h2>Get Involved</h2>
-            <p>Contact Andrew Park at akp67@cornell.edu <br>
-                Or <br>
-                Visit the Announcements Page and come to an audition!</p>
-	    </div>
-	    
-    	<?php include "footer.php";?>
-        <script src="js/blueimp-gallery.min.js"></script>
-        <script>
-            blueimp.Gallery(
-            document.getElementById('links').getElementsByTagName('a'),
-            {
-                container: '#blueimp-gallery-carousel',
-                carousel: true
-            });
-        </script>
-	</body>
-</html>
+            
+	<?php include "footer.php";?>            
+		</body>
+	</html>

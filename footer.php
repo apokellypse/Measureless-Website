ac58888074f727a3bce796c77165946244076156
Kelly Yu <euriiko@gmail.com>
2014-12-05 05:35:01 -0500

    added awesome upload form

--- footer.php
+++ footer.php
@@ -1,11 +1,9 @@
 <footer>
     
     <ul>
-        <li>
-    <a href="https://www.facebook.com/CornellMeasureless"><img src="img/iconfb.png" ></a> </li>
-       <li> <img src="img/iconyt.png" ></li>
-
-	<li>Site &copy; 1300 Group 2014</li>
-            </ul>
+       <li><a href="https://www.facebook.com/CornellMeasureless"><img src="img/iconfb.png"></a></li>
+       <li><a href="https://www.youtube.com/results?search_query=cornell+measureless"><img src="img/iconyt.png"></a></li>
+		<li>Site &copy; 1300 Group 2014 - CSSJunkies</li>
+    </ul>
 
 </footer>

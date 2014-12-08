968aa9af7849279e98cd11e3b256210a0b015306
Aaron Coven <acoven@dhcp-mcfad-2961.redrover.cornell.edu>
2014-12-08 00:39:00 -0500

    Added _target='blank'

--- footer.php
+++ footer.php
@@ -2,8 +2,8 @@
     
     <ul>
 
-       <li><a href="https://www.facebook.com/CornellMeasureless"><img src="img/iconfb.png"></a></li>
-       <li><a href="https://www.youtube.com/results?search_query=cornell+measureless"><img src="img/iconyt.png"></a></li>
+       <li><a target="_blank" href="https://www.facebook.com/CornellMeasureless"><img src="img/iconfb.png"></a></li>
+       <li><a target="_blank" href="https://www.youtube.com/results?search_query=cornell+measureless"><img src="img/iconyt.png"></a></li>
     	<li id="foottext"> Photos courtesy of Measureless</li>
 
     </ul>
